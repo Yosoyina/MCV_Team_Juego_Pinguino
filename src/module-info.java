3cf8757fc60a7445fced83d788e48c33179a68c4
MarcValentines <marcvalentinesmiro@gmail.com>
2025-04-25 16:27:57 +0200

    Añadir sql y javaFX al module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,13 @@
 module MCV_Team_Juego_Pinguino {
+	requires java.sql;
+	
+	requires transitive javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens Vista to javafx.fxml;
+	
+	exports Vista;
+	exports Controlador;
+	exports Modelo;
 }
